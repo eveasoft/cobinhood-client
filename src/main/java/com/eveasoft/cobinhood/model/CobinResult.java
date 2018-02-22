03f4202b6499f23f60c299c7bf56f74f74c03587
Guilhem <guilhem@eveasoft.com>
2018-02-22 18:05:09 +0100

    FIXed missing serialized name (candles).

--- src/main/java/com/eveasoft/cobinhood/model/CobinResult.java
+++ src/main/java/com/eveasoft/cobinhood/model/CobinResult.java
@@ -7,9 +7,9 @@ import com.google.gson.annotations.SerializedName;
  */
 public class CobinResult<T> {
 
-    @SerializedName(value="t", alternate={"currencies", "trading_pairs", "orderbook", "ticker", "trades", "orders",
-            "order", "order_history", "balances", "ledger", "deposit_addresses", "withdrawal_addresses", "withdrawal",
-            "withdrawals", "deposit", "deposits"})
+    @SerializedName(value="t", alternate={"currencies", "trading_pairs", "orderbook", "ticker", "trades", "candles",
+            "orders", "order", "order_history", "balances", "ledger", "deposit_addresses", "withdrawal_addresses",
+            "withdrawal", "withdrawals", "deposit", "deposits"})
     private T t;
 
     public T getT() {
